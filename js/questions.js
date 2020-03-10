46328861add3d8a9923bbeee031c2e3b79b1779d
bhamann-collab <brockohamann@gmail.com>
2020-03-10 10:59:27 +1030

    Created the questions

---
+++ js/questions.js
@@ -0,0 +1,102 @@
+const myQuestions = [
+    {
+      question: "Who was the inventor of Javascript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich",
+        d: "Mark Zuckerberg"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which HTML element do we store JavaScript files in?",
+      answers: {
+        a: "<style>",
+        b: "<js>",
+        c: "<javascript>",
+        d: "<script>"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: 'What is the correct JavaScript syntax to change the value of the paragraph element: <p id="demo">Please Change Me</p>',
+      answers: {
+        a: 'document.getElement("p").innerHTML = "Hello World!";',
+        b: '#demo.innerHTML = "Hello World!";',
+        c: 'document.getElementByName("p").innerHTML = "Hello World!";',
+        d: 'document.getElementById("demo").innerHTML = "Hello World!"'
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "How do you create a function In JavaScript?",
+      answers: {
+        a: "myFunction();",
+        b: "call function myFunction;",
+        c: "call myFunction();",
+        d: "var myFunction();"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: 'How do you write "Hello World" in an alert box?',
+      answers: {
+        a: 'alert("Hello World");',
+        b: 'msg("Hello World");',
+        c: 'prompt("Hello World");',
+        d: 'msgBox("Hello World");'
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: 'How do you write an IF statement in JavaScript?',
+      answers: {
+        a: 'if i==5 then {}',
+        b: 'if (i==5) {}',
+        c: 'if i=5 then {}',
+        d: 'if i=5 {}'
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: 'How do you find the number with the highest value of x and y?',
+      answers: {
+        a: 'Math.ceil(x,y)',
+        b: 'ceil(x,y)',
+        c: 'top(x,y)',
+        d: 'Math.max(x,y)'
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: 'Which of these is a valid line of JS code that stores an object in a variable?',
+      answers: {
+        a: 'var me = {age=31, hair="brown"};',
+        b: 'var me = {age: 31, hair: "brown"};',
+        c: 'var me = {age:31 hair:"brown"};',
+        d: 'var me = {var age=31, var hair="brown"};'
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: 'Which of these objects have 3 properties?',
+      answers: {
+        a: 'var me={age:31, hair:"brown"}',
+        b: 'var me={age:31}',
+        c: 'var me={age:31, hair:"brown", eyes:"blue"}',
+        d: 'var me={hair:["brown","blonde","pink"]}'
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: 'Which of these is a JavaScript package manager?',
+      answers: {
+        a: '"Node.js"',
+        b: '"TypeScript"',
+        c: '"AJAX"',
+        d: '"npm"'
+      },
+      correctAnswer: "d"
+    },
+  ];
